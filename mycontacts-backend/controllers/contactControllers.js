6ee8e6b2c4856762bcce65f1877e50e8bb13494b
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2023-12-04 13:23:52 +0530

    Created the DBControllers.

---
+++ mycontacts-backend/controllers/contactControllers.js
@@ -0,0 +1,46 @@
+//@desc get all contacts
+//route GET /api/contacts
+//@access is public will be changed to private when auth is enabled
+
+
+
+const getContacts = (req,res)=>{
+    res.status(200).json({message: "Get all Contacts"});
+}
+
+//@desc get all contacts
+//route POST /api/contacts
+//@access is public will be changed to private when auth is enabled
+const createContact =(req,res)=>{
+    res.status(201).json({message: "Create Contacts"})
+};
+//@desc get all contacts
+//route GET /api/contacts
+//@access is public will be changed to private when auth is enabled
+const getContact= (req,res)=>{
+    res.status(200).json({message: `Get Contact for ${req.params.id}`});  // to require a specific contact
+}
+//@desc get all contacts
+//route PUT /api/contacts
+//@access is public will be changed to private when auth is enabled
+
+const updateContact = (req,res)=>{
+    res.status(200).json({message: `Update Contact for ${req.params.id}`});
+}
+//@desc get all contacts
+//route DELETE /api/contacts
+//@access is public will be changed to private when auth is enabled
+const deleteContact = (req,res)=>{
+    res.status(200).json({message: `Delete Contact for ${req.params.id}`});
+}
+
+
+module.exports = {
+    getContacts, 
+    createContact, 
+    getContact, 
+    updateContact,
+    deleteContact
+
+
+}
--- mycontacts-backend/routes/contactRoutes.js
+++ mycontacts-backend/routes/contactRoutes.js
@@ -1,24 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
+const{ 
+    getContacts, 
+    createContact, 
+    getContact, 
+    updateContact,
+    deleteContact} = require("../controllers/contactControllers");
 
-router.route("/").get( (req,res)=>{
-    res.status(200).json({message: "Get all Contacts"}); // to get all contacts
-});
 
-router.route("/").post( (req,res)=>{
-    res.status(200).json({message: "Create Contacts"});  // to create a new contact
-});
+router.route("/").get( getContacts);
 
-router.route("/:id").get( (req,res)=>{
-    res.status(200).json({message: `Get Contact for ${req.params.id}`});  // to require a specific contact
-});
+router.route("/").post(createContact);
 
-router.route("/:id").put( (req,res)=>{
-    res.status(200).json({message: `Update Contact for ${req.params.id}`});  // to update a contact
-});
+router.route("/:id").get(getContact);
 
-router.route("/:id").delete( (req,res)=>{
-    res.status(200).json({message: `Delete Contact for ${req.params.id}`}); // to delete a contact
-});
+router.route("/:id").put(updateContact);
+
+router.route("/:id").delete(deleteContact);
 module.exports = router;
